650cb516dcc9e3b57db96b9e2a6bdbe13c2d6ec2
ryanwd123 <ryan.w.dewitt@gmail.com>
2024-04-06 08:39:09 -0500

    added struct, list, and map to grammer

--- test_files/t2.py
+++ test_files/t2.py
@@ -1,4 +1,15 @@
-import re
-
-
-''
+#%%
+import duckdb
+db = duckdb.connect()
+#!%load_ext dabbler.ext
+#%%
+db.sql(
+"""--sql
+select unnest([
+    [1,2,3],
+    [1,2,3],
+    [1,2,3],
+])
+"""
+)
+# %%
