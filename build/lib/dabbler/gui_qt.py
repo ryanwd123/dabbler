281c4762d2b092e01747b1fcef4047287d63c557
ryanwd123 <ryan.w.dewitt@gmail.com>
2024-07-14 14:54:44 -0500

    delete build folder

--- build/lib/dabbler/gui_qt.py
+++
@@ -1,8 +0,0 @@
-try:
-    from PySide6 import QtWidgets, QtCore, QtGui
-    from PySide6.QtCore import Qt, Signal, Slot
-    from PySide6.QtGui import QFont, QFontMetrics, QKeySequence, QShortcut
-except ImportError:
-    from qtpy import QtWidgets, QtCore, QtGui
-    from qtpy.QtCore import Qt, Signal, Slot
-    from qtpy.QtGui import QFont, QFontMetrics, QKeySequence, QShortcut
